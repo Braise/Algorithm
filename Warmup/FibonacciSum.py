7bc202e774225e8e7776a645f725bd03f36f895c
Braise <boonen.bastien@gmail.com>
2018-03-28 21:15:11 +0200

    naive partial sum

--- Warmup/FibonacciSum.py
+++ Warmup/FibonacciSum.py
@@ -1,50 +1,26 @@
 #Use python3
-def GetPisanoPeriod(m):
-    a = 0
-    b = 1
-    c = a + b
 
-    for i in range(0, m * m):
-        c = (a + b) % m
-        a = b
-        b = c
+def GetFibonacciSumNaive(n, m):
+    if(m <= 2):
+        return m
 
-        if(a == 0 and b == 1):
-            return i + 1
+    sumParam = 0
 
-def ComputFibonacciNumber(n):
-    if(n <= 1):
-        return n
+    if(n == 1):
+        sumParam = 1
 
-    a = 0
-    b = 1
-    c = 0
-    for _ in range(2, n):
-        c = a + b
-        a = b
-        b = c
+    fibo = [0, 1]
+    for i in range(2, m):
+        fibo.append(fibo[i - 1] + fibo[i - 2])
+        if(i >= n):
+            sumParam += fibo[i - 1] + fibo[i - 2]
 
-    return a + b
+    return sumParam + (fibo[m - 1] + fibo[m - 2])
 
-def GetFibonacciModulo(i, m):
-    pisanoLength = GetPisanoPeriod(m)
-    k = i % pisanoLength
-    fib = ComputFibonacciNumber(k)
-    return fib % m
-
-def GetFibonacciSum(n):
-    return GetFibonacciModulo(n + 2, 10) - 1
-
-    
 def GetPartialFibonacciSum(n, m):
-    if( n == m):
-        return ComputFibonacciNumber(n)
-
-    inf = ComputFibonacciNumber(n)
-    sup = ComputFibonacciNumber(m + 2) - 1
-    return sup - inf
-
-#print(GetFibonacciSum(100) % 10)
-#print(GetFibonacciSum(999999))
-print(GetPartialFibonacciSum(10, 10))
+    if(n == m):
+        return n
+    return GetFibonacciSumNaive(n, m)
+    
+print(GetPartialFibonacciSum(1, 3))
 input()
